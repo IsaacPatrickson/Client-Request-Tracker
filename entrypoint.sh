b89f32ca77e30876f6d116c7ded04fcdcd621cf0
Isaac <isaacspatrickson@gmail.com>
2025-07-05 12:20:35 +0100

    Go live modifications - getting the staticfiles to run in runtime container and ensuring migrations run on entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 PORT=${PORT:-8000}
 
+echo "Running migrations..."
+python manage.py migrate --noinput
+
 if [ "$DJANGO_ENV" = "production" ]; then
+    echo "Starting Gunicorn..."
     gunicorn mysite.wsgi:application --bind 0.0.0.0:$PORT
 else
+    echo "Starting Django development server..."
     python manage.py runserver 0.0.0.0:$PORT
 fi
